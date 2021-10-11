e6493f1260891c074c5a8b87df331b37c6931360
MohammadGhoffur <90598339+MohammadGhoffur@users.noreply.github.com>
2021-10-11 19:35:44 +0700

    Pembuatan Biodata

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+    class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Mohammad Ghoffur
+            Saya tingga di daerah Kabupaten Ciamis
+            olah raga yang saya sukai adalah
+            Bersepeda dan Berlari";
+    }
+}
